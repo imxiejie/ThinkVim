9fefad3d92e552b81dbbfd4971f3a860cbbba0c7
Raphael <sawakitaeiji233@gmail.com>
2020-05-18 18:54:58 +0800

    update the cleanup scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -8,15 +8,11 @@ generator="$HOME/.config/nvim/bin/generator"
 
 action "Running Clean up..."
 
-
 running "Remove the .thinkvim.d folder"
 rm -rf thinkvim_personal
 
 running "Remove the plugins install folder"
 rm -rf cache_vim
-
-running "Remove the thinkvim"
-rm -rf "$HOME/.config/nvim"
 
 ok "Clean up Success"
 
